a4b53e6b776796f02d5bc60c84b273465e52daef
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-08 02:34:36 +0300

    Added ReadMe.md

---
+++ ReadME.md
@@ -0,0 +1,8 @@
+## TODO:
+
+- Scatter Plot with d3
+- fix type error on data line 106
+
+## Stop
+
+Day 1 - Paid Accounts
--- src/plotting.ts
+++ src/plotting.ts
@@ -1,4 +1,5 @@
 import { Plot } from "d3";
+
 Plot.plot({
   marks: [
     Plot.dot(data_scatterplot, { x: "flipper_length_mm", y: "body_mass_g" }),
