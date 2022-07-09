b337d3bccc88edf3ad57be4cda8404e6e74a9028
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-09 18:51:48 +0300

    Finished Chapter 1

--- src/plotting.ts
+++ src/plotting.ts
@@ -1,6 +1,3 @@
-<<<<<<< Updated upstream
-import * as d3 from "d3";
-import * as asciichart from "asciichart";
 import { salaries_and_tenures } from "./consts";
 
 /**
@@ -12,12 +9,3 @@ const plottable_salaries_and_tenures = salaries_and_tenures.map(([salaries, tenu
 });
 
 
-=======
-import { Plot } from "d3";
-
-Plot.plot({
-  marks: [
-    Plot.dot(data_scatterplot, { x: "flipper_length_mm", y: "body_mass_g" }),
-  ],
-});
->>>>>>> Stashed changes
