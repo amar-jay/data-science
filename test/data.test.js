73bffa22a4f5412b511c7f4f319ab9e303bcd807
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-09 20:37:54 +0300

    Mocha added

---
+++ test/data.test.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect
+
+describe('Life on Earth', function() {
+  it('Adam Ate the Fruit', function(done){
+
+    expect('1').to.equal('1');
+    done();
+  });
+
+    it('Eve mom of all creation', function(done) {
+
+      expect('Eve').to.equal('Eve')
+      done();
+    })
+  })
