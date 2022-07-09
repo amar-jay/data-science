7049be95c0eb727ee9b3eb6a28b7f3555347e7a3
amar-jay <abdelmanan.abdelrahman03@gmail.com>
2022-07-09 20:53:01 +0300

    Mocha added

--- test/data.test.js
+++ test/data.test.js
@@ -1,17 +1,15 @@
 /* eslint-disable @typescript-eslint/no-undef */
 /* eslint-disable @typescript-eslint/no-var-requires */
-const expect = require('chai').expect
+const expect = require("chai").expect;
 
-describe('Life on Earth', function() {
-  it('Adam Ate the Fruit', function(done){
-
-    expect('1').to.equal('1');
+describe("Life on Earth", function () {
+  it("Adam Ate the Fruit", function (done) {
+    expect("1").to.equal("1");
     done();
   });
 
-    it('Eve mom of all creation', function(done) {
-
-      expect('Eve').to.equal('Eve')
-      done();
-    })
-  })
+  it("Eve mom of all creation", function (done) {
+    expect("Eve").to.equal("Eve");
+    done();
+  });
+});
